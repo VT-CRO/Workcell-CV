00a16626a968116fb05678445f9312d9aa67026c
ArcticWalker317 <Allproscout@gmail.com>
2025-09-28 12:39:57 -0400

    improved line 10

--- AprilTags/apriltags.py
+++ AprilTags/apriltags.py
@@ -1,5 +1,12 @@
 import apriltag
 import cv2
-img = cv2.imread('apriltag_foto.jpg'.cv2.IMREAD_GRAYSCALE)
+
+cap = cv2.VideoCapture(0) #with laptop webcam
+
+if not cap.isOpened():
+	print("Unable to open webcam")
+	exit()
+
+img = cv2.imread(cap.cv2.IMREAD_GRAYSCALE)
 detector = apriltag.Detector()
 result = detector.detect(img)
